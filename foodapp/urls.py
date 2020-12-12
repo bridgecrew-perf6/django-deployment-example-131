b57fdc59ce6c487c38ffe62c2af4f1679be8db0a
Vaibhav Kardate <vaibhavkardate@Vaibhavs-MacBook-Air.local>
2020-12-12 22:01:25 +0530

    your message

--- foodapp/urls.py
+++ foodapp/urls.py
@@ -15,7 +15,15 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from foodstaskerapp import views
+from django.contrib.auth import views as auth_views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', views.home,name='home'),
+    # path('restaurant/sign-in/',auth_views.LoginView.as_views(
+    #     template_name='restaurant/sign_in.html'),name="restaurant-sign-in"),
+    # path('restaurant/sign-out',auth_views.LogoutView.as_view(
+    #      next_page='/'),name="restaurant-sign_out"),
+    path('restaurant/',views.restaurant_home,name="restaurant-home"),
 ]
